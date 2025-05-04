11cb153d64eb9de83086dee8302d55dd1fa0b53f
CarlusInnocent <carlusinnocentainembabazi@gmail.com>
2025-05-05 00:06:23 +0300

    added carlos folder

---
+++ carlos/strings.py
@@ -0,0 +1,39 @@
+'''
+STRINGS
+"This is a string" or 'This is also a string'
+
+print(name)
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "carlos"
+last_name = "innocent"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+print("Gods must be crazy!")
+print("\tGods must be crazy!")
+print("Names:\nJob\nSimon\nJane\nHassan")
+print("Names:\n\tJob\n\tSimon\n\tJane\n\tHassan")
+
+'python' 'python ' are not the same
+
+fav_language = ' Python '
+print(fav_language)
+print(fav_language.rstrip())
+print(fav_language.lstrip())
+print(fav_language.strip())
+
+my_url = 'https://aci.com'
+print(my_url.removeprefix('https://'))
+
+name = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new"
+message = f'{name} once said, "{quote}"'
+print(message)
+
+'''
+
+
